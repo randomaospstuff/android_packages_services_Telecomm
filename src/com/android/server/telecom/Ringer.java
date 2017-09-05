1d3c9f3eb3ef80c8d08f9959f9afee0a589aa893
beanstown106 <nbenis106@gmail.com>
2017-09-05 14:35:10 +0800

    Telecomm: Incall vibration option [2/3]
    Adapted to PE
    
    commit d54535899eb9f22f9ae63bd58b55e69e36d13ecf
    Author: xyyx <xyyx@mail.ru>
    Date:   Tue Sep 5 16:02:21 2017 +0800
    
        Use double vibration on call hang-up
    
    Change-Id: I79f44f591ede74eab50d0f9c21ebf2de4ae44923
    
    commit 9dd00fdd423b9da498db694e1c7c465278a71cf7
    Author: AmolAmrit <amol.amrit03@outlook.com>
    Date:   Sat Mar 2 18:04:54 2019 +0530
    
        Telecomm: Update code for in-call vibrations
    
        * Use the same way as Omni does it: https://github.com/omnirom/android_packages_services_Telecomm/commit/39569c2a84305d3a725d3ecd41d1ab75bba6050b#diff-071431d2991cfddbeb3729657275febd
        * Makes it more robust
    
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>
    
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: I057dd3c5d862d65c3cbe18105a17e23f7790e8fc

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -29,7 +29,10 @@ import android.media.Ringtone;
 import android.media.VolumeShaper;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.os.Vibrator;
+import android.provider.Settings;
+
 import android.provider.Settings;
 
 import com.android.internal.annotations.VisibleForTesting;
@@ -106,6 +109,12 @@ public class Ringer {
             0, // No delay before starting
             255, // Vibrate full amplitude
             0, // No amplitude while waiting
+    };
+
+    private static final long[] CALL_WAITING_VIBRATION_PATTERN = {
+            200,
+            300,
+            500,
     };
 
     /**
@@ -416,6 +425,13 @@ public class Ringer {
         Log.v(this, "Playing call-waiting tone.");
 
         stopRinging();
+
+       if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.INCALL_FEEDBACK_VIBRATE, 0, UserHandle.USER_CURRENT) == 1) {
+            if (mVibrator.hasVibrator()) {
+                mVibrator.vibrate(CALL_WAITING_VIBRATION_PATTERN, -1);
+            }
+        }
 
         if (mCallWaitingPlayer == null) {
             Log.addEvent(call, LogUtils.Events.START_CALL_WAITING_TONE, reason);
